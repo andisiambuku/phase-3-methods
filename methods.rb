76888553ff334a7b446e756902350e64f265445e
andisiambuku <roselandambuku@gmail.com>
2022-10-17 13:02:48 +0300

    Assignment done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+
+#method 1
+def greet_programmer
+    puts "Hello, programmer!"
+end    
+
+#method 2
+def greet(name="Ruby")
+    puts "Hello, #{name}!"
+end
+
+#method 3
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+#method 4
+def add(num1,num2)
+    return num1+num2
+end
+
+#method 5
+def halve(num3)
+    if num3.class != Integer
+        return nil
+    end
+    
+    num3/2
+end
